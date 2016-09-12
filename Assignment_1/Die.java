b49fde83f32e79715bb4cbf8c52cd224c9e67460
Zach's Student Account <heilmazs@clarkson.edu>
2016-09-12 15:37:59 -0400

    Workin on Die.java

--- Assignment_1/Die.java
+++ Assignment_1/Die.java
@@ -1,12 +1,30 @@
-import java.util.Random;  
+/**
+ * @ author Zachary Heilman
+ * @ date 09/11/16
+ */
 
-public class Die(){
+import java.util.Random;  //Imports the Random Class
 
+/**
+ * [Die description]
+ * @return [description]
+ */
+public class Die (){
+private static int rollVal = 0;
+private static int topVal;
+/**
+ * [roll description]
+ * @return [description]
+ */
+public int roll(int sides){
+        Random r = new Random();
+        this.rollVal = r.nextInt(sides)+1;
 }
-
-public int roll(){
-
-}
+/**
+ * [getTop description]
+ * @return [description]
+ */
 public int getTop(){
 
+}
 }
